6452dd62f3197f2e04a35ce253273d91b8e3840b
Mateo <mateom.realtor@gmail.com>
2022-09-28 17:10:14 -0400

    added features to navigation bar

--- script.js
+++ script.js
@@ -2,19 +2,10 @@ const navBar = document.getElementById('navSection')[0]
 const header = document.getElementsByClassName('header')[0]
  
 
-// Function that enables scrolling functionality
+// Function that enables scrolling functionality for navigation
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky",window.scrollY>20)
 
 })
 
 
-//   const scrolled = window.scrollY;
-//     if (scrolled > 20) {
-//         header.classList.add('myStyle' )
-//       console.log(scrolled)
-//     }else{
-//         console.log("note yet")
-//     }
-   
-//     console.log(scrolled)
