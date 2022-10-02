e8ea0185667d05778c2a7e6a8d7011bc02572e40
Mateo <mateom.realtor@gmail.com>
2022-10-01 23:25:49 -0400

    Cretaed the animation for site

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navBar = document.getElementById('navSection')[0]
 const header = document.getElementsByClassName('header')[0]
- const body = document.gete
+ 
 
 // Function that enables scrolling functionality for navigation
 window.addEventListener("scroll", function () {
