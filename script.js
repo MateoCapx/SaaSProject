7339e1a1b2e5f0eec0490639b557da6a876042a4
Mateo <mateom.realtor@gmail.com>
2022-10-03 18:06:50 -0400

    Cretaed event listener for play video button

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const navBar = document.getElementById('navSection')[0]
 const header = document.getElementsByClassName('header')[0]
- 
+ const playButton =document.getElementsByClassName('fa-solid')[0]
 
 // Function that enables scrolling functionality for navigation
 window.addEventListener("scroll", function () {
     header.classList.toggle("sticky",window.scrollY>20)
-
 })
 
- 
+playButton.addEventListener("click",()=>{
+    alert("This link will direct to a demo video")
+})
+
+
 let i = 0;
 let txt = 'Lorem ipsum typing effect!'; /* The text */
 let speed = 50; /* The speed/duration of the effect in milliseconds */
@@ -21,3 +24,4 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
